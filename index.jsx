58c62fafddec2a0580058676836f694076f5ec2f
Brandon Patterson <bpatterson@mozilla.com>
2024-05-03 18:24:15 -0400

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import './App.css'; // You can define your styles in this CSS file
+
+const SplashPage = () => {
+  const [loadingProgress, setLoadingProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLoadingProgress(prevProgress => (prevProgress >= 100 ? 0 : prevProgress + 10));
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="splash-container">
+      <div className="loading-bar" style={{ width: `${loadingProgress}%` }}></div>
+      <p className="coming-soon">Coming Soon</p>
+    </div>
+  );
+};
+
+export default SplashPage;
